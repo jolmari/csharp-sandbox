55cebd3ae22985ee9bb29f31a48f694b686f0991
Kristian Virmo <kristian.virmo@devisioona.fi>
2022-12-15 21:15:46 +0200

    Reactive extensions testing

---
+++ RxTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reactive.Linq;
+
+var observable = Observable.Create<char>((obs, cancel) =>
+{
+    return Task.Run(() =>
+    {
+        while(!cancel.IsCancellationRequested)
+        {
+            var key = Console.ReadKey();
+            obs.OnNext(key.KeyChar);
+        }
+    });
+});
+
+var keys = observable
+    .Where(value => char.IsUpper(value))
+    .TakeUntil(value => value == 'Q')
+    .Publish()
+    .RefCount();
+
+keys.Subscribe(new CustomObserver<char>());
+
+var lastItem = keys.LastAsync();
+await lastItem;
+
+class CustomObserver<T> : IObserver<T>
+{
+    public void OnCompleted()
+    {
+        Console.WriteLine("Completed!");
+    }
+
+    public void OnError(Exception error)
+    {
+        Console.WriteLine("Failed!");
+    }
+
+    public void OnNext(T value)
+    {
+        Console.WriteLine( $"Received {value}" );
+    }
+}
